367cc662b6f2eaf8e778eab9560ad3b8cd8d870e
Harish Gundecha <hgundecha@users.noreply.github.com>
2026-06-04 13:57:58 -0700

    fix: set correct base URL for GitHub Pages subdirectory

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/srca-league-manager/' // 👈 Replace 'srca-league-manager' with your EXACT GitHub repository name!
 })
